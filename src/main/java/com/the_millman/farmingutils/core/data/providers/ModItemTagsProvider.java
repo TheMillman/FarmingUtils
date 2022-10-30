1b4d35b636afa4639638169ef6439c857fb9070b
TheMillman <marcopa2000@gmail.com>
2022-10-30 13:52:36 +0100

    Improvements
    Updated TheMillmanLib, deleted the guis png, added tags.

--- src/main/java/com/the_millman/farmingutils/core/data/providers/ModItemTagsProvider.java
+++ src/main/java/com/the_millman/farmingutils/core/data/providers/ModItemTagsProvider.java
@@ -3,6 +3,7 @@ package com.the_millman.farmingutils.core.data.providers;
 import com.the_millman.farmingutils.FarmingUtils;
 import com.the_millman.farmingutils.core.init.ItemInit;
 import com.the_millman.farmingutils.core.tags.ModItemTags;
+import com.the_millman.themillmanlib.core.util.LibTags;
 
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
@@ -38,6 +39,12 @@ public class ModItemTagsProvider extends ItemTagsProvider {
 			.add(ItemInit.IRON_UPGRADE.get())
 			.add(ItemInit.GOLD_UPGRADE.get())
 			.add(ItemInit.DIAMOND_UPGRADE.get());
+		
+		tag(LibTags.Items.IRON_RANGE_UPGRADE).add(ItemInit.IRON_UPGRADE.get());
+		tag(LibTags.Items.GOLD_RANGE_UPGRADE).add(ItemInit.GOLD_UPGRADE.get());
+		tag(LibTags.Items.DIAMOND_RANGE_UPGRADE).add(ItemInit.DIAMOND_UPGRADE.get());
+		tag(LibTags.Items.REDSTONE_UPGRADE).add(ItemInit.REDSTONE_UPGRADE.get());
+		tag(LibTags.Items.DROP_UPGRADE).add(ItemInit.DROP_UPGRADE.get());
 	}
 	
 	@Override
---
+++ src/main/java/com/the_millman/farmingutils/core/util/FarmingResources.java
@@ -0,0 +1,11 @@
+package com.the_millman.farmingutils.core.util;
+
+import com.the_millman.farmingutils.FarmingUtils;
+import com.the_millman.themillmanlib.core.util.ResourcesRegister;
+
+import net.minecraft.resources.ResourceLocation;
+
+public class FarmingResources extends ResourcesRegister {
+
+	public static final ResourceLocation INTERNAL_FARMER_GUI = modLoc(FarmingUtils.MODID, "internal_farmer_gui");
+}
